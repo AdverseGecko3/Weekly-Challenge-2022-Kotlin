5875ddcb0cf9e39b47dc5035de36bc6cd3f5bd88
AdverseGecko3 <eric.barrero@hotmail.com>
2022-06-29 15:23:51 +0200

    Solución Reto 26

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge26.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge26.kt
@@ -18,3 +18,55 @@ package com.mouredev.weeklychallenge2022
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
+
+fun main() {
+    drawFigure(13, 0)
+    drawFigure(12, 1)
+}
+
+fun drawFigure(size: Int, type: Int) {
+    when (type) {
+        0 -> {
+            // Triangle
+            (1..size).forEach { row ->
+                if (row == size) {
+                    (1..size).forEach { _ ->
+                        print("*  ")
+                    }
+                } else if (row > size / 2) {
+                    (1..size).forEach { col ->
+                        if (col == row || col == size - (row - 1)) {
+                            print("*  ")
+                        } else {
+                            print("   ")
+                        }
+                    }
+                } else {
+                    return@forEach
+                }
+                println()
+            }
+            println()
+        }
+        1 -> {
+            // Square
+            (1..size).forEach { row ->
+                if (row == 1 || row == size) {
+                    (1..size).forEach { _ ->
+                        print("*  ")
+                    }
+                } else {
+                    (1..size).forEach { col ->
+                        if (col == 1 || col == size) {
+                            print("*  ")
+                        } else {
+                            print("   ")
+                        }
+                    }
+                }
+                println()
+            }
+            println()
+        }
+    }
+}
